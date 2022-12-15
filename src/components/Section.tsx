35157bdd9f8f203b4e68fc5c39cbe4b0f5634b1c
ndaemy <yuyaebean@gmail.com>
2022-12-16 02:48:05 +0900

    ✨ Update navbar

---
+++ src/components/Section.tsx
@@ -0,0 +1,7 @@
+import type { FC, PropsWithChildren } from 'react';
+
+export const Section: FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <div className='mx-auto max-w-screen-lg px-6 py-4 sm:px-9'>{children}</div>
+  );
+};
--- src/partials/Navbar.tsx
+++ src/partials/Navbar.tsx
@@ -1,44 +1,44 @@
-import {
-  Logo,
-  NavbarTwoColumns,
-  NavMenu,
-  NavMenuItem,
-  Section,
-} from 'astro-boilerplate-components';
+import { Section } from '@/components/Section';
+import { AppConfig } from '@/utils/AppConfig';
 
-const Navbar = () => (
+export const Navbar = () => (
   <Section>
-    <NavbarTwoColumns>
-      <a href='/'>
-        <Logo
-          icon={
-            <svg
-              className='mr-1 h-10 w-10 stroke-cyan-600'
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 24 24'
-              fill='none'
-              strokeWidth='1.5'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-            >
-              <path d='M0 0h24v24H0z' stroke='none'></path>
-              <rect x='3' y='12' width='6' height='8' rx='1'></rect>
-              <rect x='9' y='8' width='6' height='12' rx='1'></rect>
-              <rect x='15' y='4' width='6' height='16' rx='1'></rect>
-              <path d='M4 20h14'></path>
-            </svg>
-          }
-          name="Ixartz's Blog"
-        />
+    <div className='flex flex-col items-center gap-y-2 sm:flex-row sm:justify-between'>
+      <a href='/' className='py-2'>
+        <div className='flex items-center bg-gradient-to-br from-sky-500 to-cyan-400 bg-clip-text text-xl font-bold text-transparent'>
+          {AppConfig.site_name}
+        </div>
       </a>
 
-      <NavMenu>
-        <NavMenuItem href='/posts'>Blogs</NavMenuItem>
-        <NavMenuItem href='/'>GitHub</NavMenuItem>
-        <NavMenuItem href='/'>Twitter</NavMenuItem>
-      </NavMenu>
-    </NavbarTwoColumns>
+      <nav>
+        <ul className='flex gap-x-3 font-medium text-gray-200'>
+          <li>
+            <a href='/posts' className='rounded px-3 py-2 hover:bg-slate-800'>
+              Blogs
+            </a>
+          </li>
+          <li>
+            <a
+              href='https://github.com/ndaemy'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='rounded px-3 py-2 hover:bg-slate-800'
+            >
+              GitHub
+            </a>
+          </li>
+          <li>
+            <a
+              href='https://www.linkedin.com/in/ndaemy/'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='rounded px-3 py-2 hover:bg-slate-800'
+            >
+              LinkedIn
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
   </Section>
 );
-
-export { Navbar };
