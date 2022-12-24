dc5ca1d30ecc02a93686294f113679a95abe6bbd
ndaemy <yuyaebean@gmail.com>
2022-12-24 15:56:23 +0900

    💬 Update some texts in careers

--- src/components/Home/CareerList.tsx
+++ src/components/Home/CareerList.tsx
@@ -4,18 +4,15 @@ import { ListItem } from './ListItem';
 
 export const CareerList: FC = () => (
   <ul>
-    <ListItem duration='2020.11 - Current' title='Watcha Inc.' />
-    <ListItem
-      duration='2022.11'
-      title='코드스테이츠 개발자 커뮤니케이션 특강'
-    />
+    <ListItem duration='2020.11 - Current' title='Watcha Frontend Engineer' />
+    <ListItem duration='2022.11' title='Code States 개발자 커뮤니케이션 특강' />
     <ListItem
       duration='2020.07'
       title='POSCO 청년 AI · Big Data 아카데미 강의'
     />
     <ListItem
       duration='2020.05 - 2020.10'
-      title='이노밸류파트너즈 책임연구원 / React Native 강사'
+      title='INNOVALUE Partners 책임연구원 / React Native 강사'
     />
   </ul>
 );
