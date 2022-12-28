950f91b9cdca269b2194ddc574a97bc2e209105d
ndaemy <yuyaebean@gmail.com>
2022-12-28 19:12:02 +0900

    ✨ Add empty state to BlogGallery component

--- src/components/BlogGallery.tsx
+++ src/components/BlogGallery.tsx
@@ -9,10 +9,15 @@ type Props = {
   postList: MarkdownInstance<Frontmatter>[];
 };
 
-export const BlogGallery: FC<Props> = ({ postList }) => (
-  <div className='grid grid-cols-1 gap-6 md:grid-cols-3'>
-    {postList.map(post => (
-      <BlogCard key={post.url} instance={post} />
-    ))}
-  </div>
-);
+export const BlogGallery: FC<Props> = ({ postList }) =>
+  postList.length ? (
+    <div className='grid grid-cols-1 gap-6 md:grid-cols-3'>
+      {postList.map(post => (
+        <BlogCard key={post.url} instance={post} />
+      ))}
+    </div>
+  ) : (
+    <div className='text-center'>
+      <p className='text-gray-500'>No posts found.</p>
+    </div>
+  );
